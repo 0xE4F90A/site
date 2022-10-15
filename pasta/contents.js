cbd93635372a99e9eee2b6d63946d4017494e1db
function.net <96418802+undefined444@users.noreply.github.com>
2022-10-15 14:08:02 +0900

    Add files via upload

---
+++ pasta/contents.js
@@ -0,0 +1,47 @@
+const $song = document.querySelector('#song');
+const $playSong = document.querySelector('#play-song');
+
+const $pasta = document.querySelector('#pasta');
+const $button1 = document.querySelector('#button1');
+
+const $makePasta = document.querySelector('#make-pasta');
+const $button2 = document.querySelector('#button2');
+
+const $makePasta2 = document.querySelector('#make-pasta2');
+const $button3 = document.querySelector('#button3');
+
+const $stop = document.querySelector('#stop');
+
+const $audio = document.querySelectorAll('audio');
+
+const masterPasta = (e, i) => {
+  $song.volume = 0;
+  e.load();
+  e.play();
+  setTimeout(() => {
+    $song.volume = 0.5;
+  }, i);
+}
+
+$audio.forEach(e => e.volume = 0.5);
+
+$playSong.addEventListener('click', () => {
+  $song.load();
+  $song.play();
+});
+
+$stop.addEventListener('click', () => {
+  $audio.forEach(e => e.pause());
+});
+
+$button1.addEventListener('click', () => {
+  masterPasta($pasta, 600)
+});
+
+$button2.addEventListener('click', () => {
+  masterPasta($makePasta, 1900);
+});
+
+$button3.addEventListener('click', () => {
+  masterPasta($makePasta2, 2200);
+});
